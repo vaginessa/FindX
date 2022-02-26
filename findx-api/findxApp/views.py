2e82eb7a9e291e0c4d6ce8714836e0c6daf73e19
Timoh97 <timohmugendi@gmail.com>
2022-02-26 19:37:50 +0300

    create login view function

--- findx-api/findxApp/views.py
+++ findx-api/findxApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+     '''View function to present users with account choices'''
+     title = 'Login'
+     return render(request,'registration/login.html',{'title': title})
