f915fc635bf84ca1d8974dbaf6141f8b625fe952
Timoh97 <timohmugendi@gmail.com>
2022-03-02 12:07:20 +0300

    add rest_framework and user model

--- findx-api/findxProject/settings.py
+++ findx-api/findxProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'findxApp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'findxProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'findx',
+        'USER':'tim',
+        'PASSWORD':'P@ssword',
+        'HOST':'localhost'
     }
 }
 
@@ -118,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+AUTH_USER_MODEL='users.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
