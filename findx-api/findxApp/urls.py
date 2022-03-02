0bccb4df0195652dab71db4f45e9006459b46f40
Timoh97 <timohmugendi@gmail.com>
2022-03-02 14:03:37 +0300

    add loginview and url

--- findx-api/findxApp/urls.py
+++ findx-api/findxApp/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path,include
-from .views import RegisterView
+from .views import *
 
 urlpatterns = [
    
-    path('register/',RegisterView.as_view())
+    path('register/',RegisterView.as_view()),
+    path('login/',LoginView.as_view())
 ]
--- findx-api/findxApp/views.py
+++ findx-api/findxApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from . serializers import UserSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 class RegisterView(APIView):
@@ -8,4 +9,8 @@ class RegisterView(APIView):
         serializer=UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Respo
+        return Response(serializer.data)
+    
+class LoginView(APIView):
+    def post(self,request):
+        pass
